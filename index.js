ca89e252fca32c85e35df69d46cbd21d51f1afce
masindes <maasinde.sylvester@gmail.com>
2024-10-25 10:59:46 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+function createTimeInEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ');
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+
+    return employee;
+}
+function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ');
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+
+    return employee;
+}
+function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(event => event.date === date);
+    let timeOut = employee.timeOutEvents.find(event => event.date === date);
+
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+function wagesEarnedOnDate(employee, date) {
+    let hours = hoursWorkedOnDate(employee, date);
+    return hours * employee.payPerHour;
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
